706458f75241cf1598f85e3d242e8d9b41d19fe5
jobin-softwaresystems <jobin@softwaresystems.ae>
2017-04-10 15:33:10 +1000

    Simple implementation of self hosting

---
+++ KatanaHost/Program.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Owin.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KatanaHost
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (WebApp.Start<Startup>("http://localhost:12345"))
+            {
+
+                Console.WriteLine("Its alive!");
+                Console.WriteLine("Oh Its alive!");
+                Console.WriteLine("Press any key to quit.");
+                Console.ReadLine();
+            }
+
+        }
+    }
+}
